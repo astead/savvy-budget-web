730f4be672760bccb25985bdd6f8c3497d5ea7e9
Alan Stead <alan.stead@gmail.com>
2024-11-08 18:18:28 -0800

    A bit better formatting for going over in the budget.

--- src/helpers/BorderLinearProgress.tsx
+++ src/helpers/BorderLinearProgress.tsx
@@ -9,9 +9,25 @@ interface ProgressBarProps {
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 10,
-  borderRadius: 5,
+  borderRadius: '5px 5px 5px 5px',
   '& .MuiLinearProgress-bar': {
-    borderRadius: 5,
+    borderRadius: '5px 5px 5px 5px',
+  },
+}));
+
+const BorderLinearProgressNoRightRadius = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: '5px 0 0 5px',
+  '& .MuiLinearProgress-bar': {
+    borderRadius: '5px 0 0 5px',
+  },
+}));
+
+const BorderLinearProgressNoLeftRadius = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: '0 5px 5px 0',
+  '& .MuiLinearProgress-bar': {
+    borderRadius: '0 5px 5px 0',
   },
 }));
 
@@ -21,25 +37,36 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ actual, target, overColor }) 
   return (
     <Box position="relative" display="flex" alignItems="center" width="100%">
       <Box width="100%" mr={1} position="relative">
-        <BorderLinearProgress
-          variant="determinate"
-          value={primaryValue}
-          sx={{ 
-            width: `${ actual > target ? ( 100 * (target / actual) ) : 100 }%`,
-          }}
-        />
-        {actual > target && (
+        {actual <= target && (
           <BorderLinearProgress
             variant="determinate"
-            value={100}
-            sx={{
-              position: 'absolute',
-              top: 0,
-              left: `${100 * (target / actual)}%`,
-              width: `${100 - ( 100 * (target / actual) )}%`,
-              '& .MuiLinearProgress-bar': { backgroundColor: overColor },
+            value={primaryValue}
+            sx={{ 
+              width: `${ actual > target ? ( 100 * (target / actual) ) : 100 }%`,
             }}
           />
+        )}
+        {actual > target && (
+          <>
+            <BorderLinearProgressNoRightRadius
+              variant="determinate"
+              value={primaryValue}
+              sx={{ 
+                width: `${ actual > target ? ( 100 * (target / actual) ) : 100 }%`,
+              }}
+            />
+            <BorderLinearProgressNoLeftRadius
+              variant="determinate"
+              value={100}
+              sx={{
+                position: 'absolute',
+                top: 0,
+                left: `${100 * (target / actual)}%`,
+                width: `${100 - ( 100 * (target / actual) )}%`,
+                '& .MuiLinearProgress-bar': { backgroundColor: overColor },
+              }}
+            />
+          </>
         )}
       </Box>
     </Box>
