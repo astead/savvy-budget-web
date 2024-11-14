b12bf7a99db554429f48ba67b82492f5d33da314
Alan Stead <alan.stead@gmail.com>
2024-11-13 19:32:34 -0800

    Remove unused stuff

--- src/helpers/BorderLinearProgress.tsx
+++ src/helpers/BorderLinearProgress.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, LinearProgress, styled, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 interface ProgressBarProps {
   actual: number;
